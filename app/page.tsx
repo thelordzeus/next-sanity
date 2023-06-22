62f7373e330e3045907088311bc801b123834ed1
thelordzeus <arnav55125@gmail.com>
2023-06-23 00:07:29 +0530

    feat: added project hover effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getProjects } from "@/sanity/utils/fetchData";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const projects = await getProjects();
@@ -17,23 +18,24 @@ export default async function Home() {
       <h2 className="mt-24 font-bold text-gray-300 text-3xl">My Projects</h2>
       <div className=" mt-5 grid md:grid-cols-2 lg:grid-col-3 gap-8">
         {projects.map((project) => (
-          <div
+          <Link
+            href={`/projects/${project.slug}`}
             key={project._id}
-            className="border border-gray-500 rounded-lg p-3"
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition duration-500 ease-in-out"
           >
             {project.image && (
               <Image
                 src={project.image}
                 alt={project.title}
-                width={250}
-                height={100}
+                width={750}
+                height={300}
                 className="object-cover rounded-lg border border-gray-500"
               />
             )}
-            <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent ">
+            <div className="mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent ">
               {project.title}
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
