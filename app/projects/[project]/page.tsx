20f37cb4d545b75196455dd1afb0e32dfaed3cdd
thelordzeus <arnav55125@gmail.com>
2023-06-23 00:51:20 +0530

    feat: added content to project page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,5 +1,6 @@
 import { getProject } from "@/sanity/utils/fetchData";
 import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 type Props = {
   params: { project: string };
@@ -25,13 +26,20 @@ export default async function Project({ params }: Props) {
           View Project
         </a>
       </header>
+      {/* image goes here  */}
+
+      <Image
+        src={project.image}
+        alt={project.title}
+        width={1920}
+        height={1080}
+        className="mt-10 border-2 border-gray-700 object-cover rounded-xl"
+      />
 
       {/* content goes here  */}
-      <div>
+      <div className="text-lg text-gray-700 mt-5 prose">
         <PortableText value={project.content} />
       </div>
-
-      {/* image goes here  */}
     </div>
   );
 }
