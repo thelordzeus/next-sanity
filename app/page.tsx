eaeb705e86259d8edd0f25b19188a8e60847b856
thelordzeus <arnav55125@gmail.com>
2023-06-22 23:57:41 +0530

    feat: added project styles

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     <div className="max-w-5xl mx-auto py-20">
       <h1 className="text-7xl font-extrabold">
         Hello I'm{" "}
-        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-puprple-600 bg-clip-text text-transparent">
+        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
           Zeus!
         </span>
       </h1>
@@ -15,22 +15,27 @@ export default async function Home() {
         Hello everyone! check out my projects!
       </p>
       <h2 className="mt-24 font-bold text-gray-300 text-3xl">My Projects</h2>
-      {projects.map((project) => (
-        <div key={project._id} className="border border-gray-500 rounded-lg ">
-          {project.image && (
-            <Image
-              src={project.image}
-              alt={project.title}
-              width={250}
-              height={100}
-              className="object-cover rounded-lg border border-gray-500"
-            />
-          )}
-          <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-puprple-600 bg-clip-text text-transparent">
-            {project.title}
+      <div className=" mt-5 grid md:grid-cols-2 lg:grid-col-3 gap-8">
+        {projects.map((project) => (
+          <div
+            key={project._id}
+            className="border border-gray-500 rounded-lg p-3"
+          >
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.title}
+                width={250}
+                height={100}
+                className="object-cover rounded-lg border border-gray-500"
+              />
+            )}
+            <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent ">
+              {project.title}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
