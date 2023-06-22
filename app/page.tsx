02e04d88e060505fb4d09bdd380312d4c50841c3
thelordzeus <arnav55125@gmail.com>
2023-06-23 00:58:20 +0530

    feat: added Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default async function Home() {
   const projects = await getProjects();
   return (
-    <div className="max-w-5xl mx-auto py-20">
+    <div className="">
       <h1 className="text-7xl font-extrabold">
         Hello I'm{" "}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -10,7 +10,7 @@ export default async function Project({ params }: Props) {
 
   const project = await getProject(slug);
   return (
-    <div className="max-w-3xl mx-auto py-20">
+    <div className="">
       <header className="flex items-center justify-between ">
         <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl  drop-shadow-sm font-extrabold">
           {project.title}
