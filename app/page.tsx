4a32104f58e6186058c6a4709bf0a8ad68133c1e
thelordzeus <arnav55125@gmail.com>
2023-06-23 02:11:13 +0530

    minor deployment fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <div className="">
       <h1 className="text-7xl font-extrabold">
-        Hello I'm{" "}
+        Hello I&apos;m{" "}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
           Zeus!
         </span>
